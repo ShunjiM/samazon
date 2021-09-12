9ac13ea76c8a0b5c0cfeb52c021227f819dba489
隼司 <mtshun.0107.4505@gmail.com>
2021-09-12 06:04:13 +0000

    04 create products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,10 +34,3 @@ major_category_names.each do |major_category_name|
     end
   end
 end
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
